61b6cf0f671d79f95a4783848e750c3358b004b9
Luke Davis <lukelbd@gmail.com>
2019-01-04 01:15:51 -0700

    Isentropic slopes

--- isentropes.py
+++ isentropes.py
@@ -33,9 +33,10 @@ else:
 eps = 1
 # thlev = np.array([*range(260, 360, 10), *range(360, 400, 20), *range(400, 800, 50), *range(800, 1600, 200)])
 # thlev = np.array([*range(260, 360, 10), *range(360, 400, 20), *range(400, 800, 50)])
-thlev = np.array([*range(300, 360, 10), *range(360, 400, 20)])
+# thlev = np.array([*range(300, 360, 10), *range(360, 400, 20)])
+thlev = np.arange(240,400,20) # must be ascending!
 # Coordinates
-times = data['time']
+time = data['time']
 plev = data['plev']
 lat = data['lat']
 lon = data['lon']
@@ -53,9 +54,8 @@ plev = plev.values.astype('float32')*units.hectopascals
 # NOTE: If temps out of bounds get error:
 # raise ValueError('Input theta level out of data bounds')
 # This should just be a warning, or optionally disabled!
-t, u, v = t[0], u[0], v[0] # index first row, i.e. first timestep
-p, t_th, u_th, v_th = mcalc.isentropic_interpolation(thlev, plev, t.values*units.kelvin, u.values, v.values, tmpk_out=True) # output isentropes in Kelvin
-dims = ('thlev', 'lat', 'lon')
+p, t_th, u_th, v_th = mcalc.isentropic_interpolation(thlev, plev, t.values*units.kelvin, u.values, v.values, axis=1, tmpk_out=True) # output isentropes in Kelvin
+dims = ('time', 'thlev', 'lat', 'lon')
 p = xr.Variable(dims, p.astype('float32'), {'long_name':'pressure', 'units':'hPa'})
 t_th = xr.Variable(dims, t_th.astype('float32'), t.attrs)
 u_th = xr.Variable(dims, u_th.astype('float32'), u.attrs)
@@ -63,7 +63,7 @@ v_th = xr.Variable(dims, v_th.astype('float32'), v.attrs)
 # Make dataset, add variables and coordinates
 # NOTE: Put coordinates in first, so they come first in ncdump
 thlev = xr.Variable('thlev', thlev, {'long_name':'potential_temperature', 'units':'K'})
-out = xr.Dataset({}, coords={'thlev':thlev, 'lat':lat, 'lon':lon})
+out = xr.Dataset({}, coords={'time':time, 'thlev':thlev, 'lat':lat, 'lon':lon})
 for name,data in zip(('p','t','u','v'),(p,t_th,u_th,v_th)):
     out[name] = data
 # out = xr.Dataset({'p':p, 't':t_th, 'u':u_th, 'v':v_th},
