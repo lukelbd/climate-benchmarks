51a0ae7aa935d5f072e24b2d7138653df22b609c
Luke Davis <lukelbd@gmail.com>
2021-06-03 17:28:13 -0600

    Minor example cleanup

--- fluxes/xarray.py
+++ fluxes/xarray.py
@@ -11,29 +11,35 @@
 #     return module
 import os
 import sys
-import imp
+
 import xarray as xr
+
 filename = sys.argv[1]
-nt = int(sys.argv[2]) # number of time chunks; 0 to disable chunking
-if nt: # 1 chunk per level
-    N = 100000 # max chunk size for lon/lat dimensions; want to chunk each 2D slice separately
-    # N = 1000 # this made no difference
-    chunks = {'time':nt, 'plev':N, 'lat':N, 'lon':N} # one chunk per horizontal slice, works pretty well
+nt = int(sys.argv[2])  # number of time chunks; 0 to disable chunking
+if nt:  # 1 chunk per level
+    # Chunk each 3D slice separately. So make max chunk size along other dims big.
+    N = 100000  # max chunk size for lon/lat dimensions
+    chunks = {
+        'time': nt,
+        'plev': N,
+        'lat': N,
+        'lon': N,
+    }  # one chunk per horizontal slice, works pretty well
     data = xr.open_dataset(filename, chunks=chunks, decode_times=False)
 else:
     data = xr.open_dataset(filename, decode_times=False)
 
 # Next perform calculations
-emf = ((data['u'] - data['u'].mean('lon')) * (data['v'] - data['v'].mean('lon'))).mean('lon')
-ehf = ((data['t'] - data['t'].mean('lon')) * (data['v'] - data['v'].mean('lon'))).mean('lon')
+emf = ((data.u - data.u.mean('lon')) * (data.v - data.v.mean('lon'))).mean('lon')
+ehf = ((data.t - data.t.mean('lon')) * (data.v - data.v.mean('lon'))).mean('lon')
 emf.name = 'emf'
 ehf.name = 'ehf'
-emf.attrs = {'long_name':'eddy momentum flux', 'units':'m**2/s**2'}
-ehf.attrs = {'long_name':'eddy heat flux', 'units':'K*m/s'}
+emf.attrs = {'long_name': 'eddy momentum flux', 'units': 'm**2/s**2'}
+ehf.attrs = {'long_name': 'eddy heat flux', 'units': 'K*m/s'}
 
 # Save file
-out = xr.Dataset({'emf':emf, 'ehf':ehf})
+out = xr.Dataset({'emf': emf, 'ehf': ehf})
 outname = f'out/xarray_{nt}.nc'
 if os.path.exists(outname):
     os.remove(outname)
-out.to_netcdf(outname, mode='w') # specify whether we did chunking
+out.to_netcdf(outname, mode='w')  # specify whether we did chunking
